18725badb84d421b158b734059959a6af2c8facf
Kamran Ali <kamranleembo12@gmail.com>
2022-12-20 01:25:02 +0500

    c

--- api/search.js
+++ api/search.js
@@ -6,46 +6,22 @@ fetch(
     return response.json();
   })
   .then((data) => {
-    data.map((count) => {
-      allCountrie = document.querySelector(".cardtwo-countries");
-      const div = document.createElement("div");
-      div.innerHTML = `<div class="countries-list">
-      <p class="countries-name">${count.Country}</p>
-      <p class="serach-numbr">${count.ActiveCases}</p>
-    </div>`;
-      allCountrie.appendChild(div);
-    });
     suggest = [];
-
     data.map((data) => {
       suggest.push(data.Country);
     });
     console.log(suggest);
-
     const input = document.querySelector(".searchdata");
-    input.onkeyup = (e) => {
-      let userData = e.target.value;
-      let emptyArray = [];
-      if (userData) {
-        emptyArray = suggest.filter((data) => {
-          return data
-            .toLocaleLowerCase()
-            .startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data) => {
-          return data;
-        });
-      }
-      console.log(emptyArray);
-      data.map((data) => {
-        const div = document.querySelector(".countries-list");
-        emptyArray.map((nm) => {
-          if (data.Country === nm) {
-            console.log(data);
-          } else {
-          }
-        });
-      });
-    };
+
+    data.map((count) => {
+      allCountrie = document.querySelector(".cardtwo-countries");
+      const div = document.createElement("div");
+
+      div.innerHTML = `<div class="countries-list">
+        <p class="countries-name">${count.Country}</p>
+        <p class="serach-numbr">${count.ActiveCases}</p>
+       </div>`;
+      allCountrie.appendChild(div);
+    });
   })
   .catch((err) => console.error(err));
